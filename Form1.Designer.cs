7aa35f3f143c660ce170d958e4321831451c338f
duke0815 <kasser88@outlook.com>
2025-01-06 16:27:33 +0100

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace PicView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.kopierenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.labelFilename = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.BackgroundImage = global::PicView.Properties.Resources.picViewer_icon;
+            this.pictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox.ContextMenuStrip = this.contextMenuStrip1;
+            this.pictureBox.Location = new System.Drawing.Point(1, 38);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(818, 448);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kopierenToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(139, 28);
+            // 
+            // kopierenToolStripMenuItem
+            // 
+            this.kopierenToolStripMenuItem.Name = "kopierenToolStripMenuItem";
+            this.kopierenToolStripMenuItem.Size = new System.Drawing.Size(138, 24);
+            this.kopierenToolStripMenuItem.Text = "Kopieren";
+            this.kopierenToolStripMenuItem.Click += new System.EventHandler(this.kopierenToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(12, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Öffnen";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(184, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 32);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Drehen";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button3.BackColor = System.Drawing.Color.Silver;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(356, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(166, 32);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Spiegeln";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button4.BackColor = System.Drawing.Color.Silver;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button4.Location = new System.Drawing.Point(719, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(92, 32);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "nächstes";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button5.BackColor = System.Drawing.Color.Silver;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button5.Location = new System.Drawing.Point(621, 3);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(92, 32);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "vorheriges";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // labelFilename
+            // 
+            this.labelFilename.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelFilename.AutoSize = true;
+            this.labelFilename.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.labelFilename.Location = new System.Drawing.Point(3, 489);
+            this.labelFilename.Name = "labelFilename";
+            this.labelFilename.Size = new System.Drawing.Size(42, 16);
+            this.labelFilename.TabIndex = 6;
+            this.labelFilename.Text = "no file";
+            this.labelFilename.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelFilename.Click += new System.EventHandler(this.labelFilename_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(820, 510);
+            this.Controls.Add(this.labelFilename);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(820, 200);
+            this.Name = "Form1";
+            this.Text = "PicViewer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem kopierenToolStripMenuItem;
+        private System.Windows.Forms.Label labelFilename;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PicView
+{
+    public partial class Form1 : Form
+    {
+        private string currentImagePath;
+        private string[] imageFiles;
+        private int currentIndex = -1;
+        private float zoomFactor = 1.0f;
+        public Form1(string[] args)
+        {
+            InitializeComponent();
+            InitializeContextMenu();
+
+            // Prüfen, ob ein Argument (Dateipfad) übergeben wurde
+            if (args.Length > 0 && File.Exists(args[0]))
+            {
+                pictureBox.BackgroundImage = null;
+                LoadImage(args[0]); // Lade das Bild
+                LoadImagesInFolder(Path.GetDirectoryName(args[0])); // Lade alle Bilder im Ordner
+                LoadImage(args[0]);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Bilddateien|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    LoadImagesInFolder(Path.GetDirectoryName(ofd.FileName)); // Ordner laden und sortieren
+                    LoadImage(ofd.FileName); // Bild laden und Position in Liste setzen
+                    pictureBox.BackgroundImage = null;
+                }
+            }
+        }
+
+
+        private void InitializeContextMenu()
+        {
+            contextMenuStrip1 = new ContextMenuStrip();
+            ToolStripMenuItem copyItem = new ToolStripMenuItem("Kopieren");
+            copyItem.Click += CopyImageToClipboard; // Ereignis-Handler wird zugewiesen
+            contextMenuStrip1.Items.Add(copyItem);
+        }
+
+
+        private void LoadImage(string path)
+        {
+            // Aktuelles Bild freigeben, falls vorhanden
+            if (pictureBox.Image != null)
+            {
+                pictureBox.BackgroundImage = null;
+                pictureBox.Image.Dispose(); // Bild freigeben
+                pictureBox.Image = null;    // Referenz löschen
+            }
+
+            currentImagePath = path;
+
+            // Alphabetischen Index aktualisieren
+            if (imageFiles != null)
+            {
+                imageFiles = imageFiles.OrderBy(f => f).ToArray(); // Alphabetisch sortieren
+                currentIndex = Array.IndexOf(imageFiles, path);
+            }
+
+            zoomFactor = 1.0f; // Zoom zurücksetzen
+
+            // Neues Bild laden
+            pictureBox.Image = Image.FromFile(path);
+
+            labelFilename.Text = Path.GetFileName(path);
+        }
+
+
+
+
+
+        private void LoadImagesInFolder(string folderPath)
+        {
+            imageFiles = Directory.GetFiles(folderPath, "*.*")
+                .Where(f => f.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase) ||
+                            f.EndsWith(".jpeg", StringComparison.OrdinalIgnoreCase) ||
+                            f.EndsWith(".png", StringComparison.OrdinalIgnoreCase) ||
+                            f.EndsWith(".bmp", StringComparison.OrdinalIgnoreCase) ||
+                            f.EndsWith(".gif", StringComparison.OrdinalIgnoreCase))
+                .OrderBy(f => f) // Alphabetisch sortieren
+                .ToArray();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox.Image != null)
+            {
+                pictureBox.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                pictureBox.Refresh();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (pictureBox.Image != null)
+            {
+                pictureBox.Image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                pictureBox.Refresh();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (imageFiles != null && currentIndex < imageFiles.Length - 1)
+            {
+                LoadImage(imageFiles[++currentIndex]); // Lade das nächste Bild basierend auf dem aktuellen Index
+            }
+            else
+            {
+                MessageBox.Show("Keine weiteren Bilder vorhanden.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (imageFiles != null && currentIndex > 0)
+            {
+                LoadImage(imageFiles[--currentIndex]); // Lade das vorherige Bild basierend auf dem aktuellen Index
+            }
+            else
+            {
+                MessageBox.Show("Keine vorherigen Bilder vorhanden.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+
+
+        private void CopyImageToClipboard(object sender, EventArgs e)
+        {
+        }
+
+        private void kopierenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+                        if (pictureBox.Image != null)
+            {
+                Clipboard.SetImage(pictureBox.Image); // Bild in die Zwischenablage kopieren
+            }
+            else
+            {
+                MessageBox.Show("Kein Bild zum Kopieren verfügbar!", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void labelFilename_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
